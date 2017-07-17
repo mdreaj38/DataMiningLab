8684dafa42ced9ccdd94f89d5bcc6d5f6c4bb763
Student-05 <student-05@testsite.com>
2017-07-17 16:47:22 +0600

    first lab

---
+++ bPlusTree/Main.java
@@ -0,0 +1,11 @@
+package bPlusTree;
+
+public class Main {
+	public static void main(String args[]){
+		BPlusTree bpt = new BPlusTree(4);
+		bpt.insert(4);
+		bpt.insert(3);
+		bpt.insert(1);
+		bpt.insert(2);
+	}
+}
