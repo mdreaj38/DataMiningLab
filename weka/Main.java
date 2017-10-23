1f3c265392e30b9237e3b60a336a8f8d62a0fb1e
Student-05 <student-05@testsite.com>
2017-10-23 16:52:22 +0600

    csv2arff

--- weka/Main.java
+++ weka/Main.java
@@ -1,9 +1,29 @@
 package DataMiningLab.weka;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) throws Exception{
+		//menu();
+		//csv2arff();
+	}
+	public static void csv2arff() throws IOException{
+		FileWriter fw = new FileWriter(new File("data/household_power_consumption_data.txt"));
+		Scanner s = new Scanner(new File("data/household_power_consumption.txt"));
+		System.out.println(s.next());
+		while(s.hasNext()){
+			String text = s.next();
+			text = text.replaceAll(";", ",");
+			//System.out.println(text);
+			fw.append(text+"\n");
+		}
+		fw.close();
+		s.close();
+	}
+	public static void menu() throws Exception{
 		Scanner s = new Scanner(System.in);
 		boolean running = true;
 		while(running){
